87ca20ceab8476d9058e6fe04ada0acbef409561
Fabian Neuschmidt <fabian@neuschmidt.de>
2025-01-11 21:22:05 +0100

    feat: two routes

--- app/Main.hs
+++ app/Main.hs
@@ -6,18 +6,34 @@
 module Main where
 
 import Data.Text (Text)
+import Effectful
 import Web.Hyperbole
 
 main :: IO ()
 main = do
   run 3000 $ do
-    liveApp (basicDocument "Example") (runPage page)
+    liveApp (basicDocument "Hlights") (routeRequest router)
+  where
+    router :: (Hyperbole :> es) => AppRoute -> Eff es Response
+    router Home = runPage page
+    router About = runPage page2
+
+data AppRoute = Home | About deriving (Eq, Generic, Show)
+
+instance Route AppRoute where
+  baseRoute = Just Home
 
 page :: (Hyperbole :> es) => Eff es (Page '[Message])
 page = do
   pure $ col id $ do
     hyper Message1 $ messageView "Hello"
     hyper Message2 $ messageView "World!"
+
+page2 :: (Hyperbole :> es) => Eff es (Page '[Message])
+page2 = do
+  pure $ col id $ do
+    hyper Message1 $ messageView "Hello2"
+    hyper Message2 $ messageView "World2"
 
 data Message = Message1 | Message2
   deriving (Show, Read, ViewId)
