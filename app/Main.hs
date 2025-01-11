c3037081d71820b9ecd90b7df1673bdf1579f341
Fabian Neuschmidt <fabian@neuschmidt.de>
2025-01-11 20:54:18 +0100

    feat: basic hyperbole

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,37 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Main where
 
-import Hlights qualified (someFunc)
+import Data.Text (Text)
+import Web.Hyperbole
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  Hlights.someFunc
+  run 3000 $ do
+    liveApp (basicDocument "Example") (runPage page)
+
+page :: (Hyperbole :> es) => Eff es (Page '[Message])
+page = do
+  pure $ col id $ do
+    hyper Message1 $ messageView "Hello"
+    hyper Message2 $ messageView "World!"
+
+data Message = Message1 | Message2
+  deriving (Show, Read, ViewId)
+
+instance HyperView Message es where
+  data Action Message = Louder Text
+    deriving (Show, Read, ViewAction)
+
+  update (Louder msg) = do
+    let new = msg <> "!"
+    pure $ messageView new
+
+messageView :: Text -> View Message ()
+messageView msg = do
+  row id $ do
+    button (Louder msg) id "Louder"
+    el_ $ text msg
