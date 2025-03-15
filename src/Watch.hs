2e0bd414ff316b6fb19a4bb227dd1f7d983a548f
Fabian Neuschmidt <fabian@neuschmidt.de>
2025-03-15 17:18:21 +0100

    Revert "feat: set lightLevel and color in single request"
    This reverts commit c3e5e1c8745d8aad069afe8c339984d7d4c6de01.

--- src/Watch.hs
+++ src/Watch.hs
@@ -72,9 +72,9 @@ clientApp env conn = do
                       let schedule = fromMaybe (Schedule {scheduleId = 0, lampId = "", timeOfDay = TimeOfDay 0 0 0, brightness = 100, colorTemperature = 3000, allowBrighten = True, allowDarken = True}) maybeSchedule
                       let deviceSet = DeviceSet {name = lampName, id = lampId}
                       liftIO $ threadDelay $ 100 * 1000
-                      setLightLevel deviceSet schedule.brightness 1000
+                      setColorTemperature deviceSet schedule.colorTemperature 200
                       liftIO $ threadDelay $ 200 * 1000
-                      setColorTemperature deviceSet schedule.colorTemperature 500
+                      setLightLevel deviceSet schedule.brightness 1000
                       liftIO $ threadDelay $ 100 * 1000
 
   -- Send a ping every 30s
